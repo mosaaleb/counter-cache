dd1ccf3a21ba8c7ab9daf3992b6ab7ed90381604
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-04-06 22:34:38 +0200

    add ability to initialize all models model with initially empty array to save counter cached columns

--- lib/counter_cache/has_counter_cache.rb
+++ lib/counter_cache/has_counter_cache.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
+require 'active_support/concern'
+
 module CounterCache
   module HasCounterCache
+    extend ActiveSupport::Concern
+
+    def counter_cached_columns
+      @counter_cached_columns ||= []
+    end
   end
 end
--- test/has_counter_cache_test.rb
+++ test/has_counter_cache_test.rb
@@ -2,5 +2,12 @@
 
 require 'test_helper'
 
-class CounterCache::HasCounterCache::Test < ActiveSupport::TestCase
+class HasCounterCacheTest < ActiveSupport::TestCase
+  def setup
+    @post = Post.new(text: 'John Snow is back!')
+  end
+
+  test 'has empty array of counter cached columns' do
+    assert_equal [], @post.counter_cached_columns
+  end
 end
