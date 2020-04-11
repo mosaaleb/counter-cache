45238709c7053d3241ebcaec51d5296120743da6
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-04-11 20:56:58 +0200

    add comments table and model for testing counting

--- lib/counter_cache/railtie.rb
+++ lib/counter_cache/railtie.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module CounterCache
   class Railtie < ::Rails::Railtie
   end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -1,9 +1,32 @@
 # frozen_string_literal: true
 
-ActiveRecord::Schema.define(version: 20_200_406_194_809) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you
+# check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20_200_411_130_608) do
+  create_table 'comments', force: :cascade do |t|
+    t.string 'text'
+    t.integer 'post_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['post_id'], name: 'index_comments_on_post_id'
+  end
+
   create_table 'posts', force: :cascade do |t|
     t.string 'text'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
   end
+
+  add_foreign_key 'comments', 'posts'
 end
