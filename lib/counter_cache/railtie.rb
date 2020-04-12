b1154c8cf00c5e88bf6cce58da9f52f9dd70314a
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-04-12 02:46:52 +0200

    add railtie initializer to automatically include counter_cache

--- lib/counter_cache/railtie.rb
+++ lib/counter_cache/railtie.rb
@@ -2,5 +2,10 @@
 
 module CounterCache
   class Railtie < ::Rails::Railtie
+    initializer 'counter cache' do
+      ActiveSupport.on_load(:active_record) do
+        ApplicationRecord.include CounterCache::HasCounterCache
+      end
+    end
   end
 end
--- test/dummy/app/models/application_record.rb
+++ test/dummy/app/models/application_record.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationRecord < ActiveRecord::Base
-  include CounterCache::HasCounterCache
-
   self.abstract_class = true
 end
