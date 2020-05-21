b7ac4c78c0aece435853be3cf18345a1cacf7518
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-05-21 23:39:36 +0200

    load counter_cache tasks on loading rails

--- lib/counter_cache/railtie.rb
+++ lib/counter_cache/railtie.rb
@@ -2,10 +2,14 @@
 
 module CounterCache
   class Railtie < ::Rails::Railtie
-    initializer 'counter cache' do
+    initializer 'include counter cache module to application record on load' do
       ActiveSupport.on_load(:active_record) do
         ApplicationRecord.include CounterCache::HasCounterCache
       end
+    end
+
+    rake_tasks do
+      load 'tasks/counter_cache_tasks.rake'
     end
   end
 end
